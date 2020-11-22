f2e1cf492ca36ee011c206a8cfc6e055225655d0
Rao Fahad <raofahad046@gmail.com>
2020-11-22 20:45:44 +0500

    updated src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p><br/>
+        <p>Update By Rao</p>
         <a
           className="App-link"
           href="https://reactjs.org"
